8a2250a34f09255a668461ad6bde3159e381a2b9
gmanandmarbles <119905567+gmanandmarbles@users.noreply.github.com>
2023-06-22 07:03:52 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -30,8 +30,8 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['myst_parser']
+
 source_suffix = {
     '.rst': 'restructuredtext',
     '.txt': 'restructuredtext',
